f019650f95a6f8c86bf160c9a5130739c31eeb03
Benson Imoh,ST <benson@stbensonimoh.com>
2021-07-09 17:22:38 +0100

    feat: Add Mobile Responsiveness

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,5 +1,7 @@
 import React from "react";
 import {
+  LandingPageComponent,
+  LandingPageContainer,
   VolunterSplash,
   Copyright,
   TitleText,
@@ -10,28 +12,31 @@ import {
 } from "./landingpage-style";
 
 function LandingPage() {
-  const date = new Date();
   return (
-    <>
-      <VolunterSplash>
-        <Logo src="/images/logo.png" />
-        <TitleText>Volunteer</TitleText>
-        <SubtitleText>
-          noun <span>/vɑː.lənˈtɪr/</span>
-        </SubtitleText>
-        <Definition>
-          A special person who lends a helping hand, a listening ear, offers
-          care and support to those in need. They dedicate their time, energy,
-          skills and knowledge to make a difference. Someone who is caring,
-          generous, thoughtful and selfless.
-        </Definition>
-        <SubtitleText type="other-words">
-          See also: ninja, magician
-        </SubtitleText>
-        <Copyright>&copy; {date.getFullYear()} Nyuma Harrison Foundation</Copyright>
-      </VolunterSplash>
-      <WarningWrapper></WarningWrapper>
-    </>
+    <LandingPageComponent>
+      <LandingPageContainer>
+        <VolunterSplash>
+          <Logo src="/images/logo.png" />
+          <TitleText>Volunteer</TitleText>
+          <SubtitleText>
+            noun <span>/vɑː.lənˈtɪr/</span>
+          </SubtitleText>
+          <Definition>
+            A special person who lends a helping hand, a listening ear, offers
+            care and support to those in need. They dedicate their time, energy,
+            skills and knowledge to make a difference. Someone who is caring,
+            generous, thoughtful and selfless.
+          </Definition>
+          <SubtitleText type="other-words">
+            See also: ninja, magician
+          </SubtitleText>
+        </VolunterSplash>
+        <WarningWrapper></WarningWrapper>
+      </LandingPageContainer>
+      <Copyright>
+        &copy; {new Date().getFullYear()} Nyuma Harrison Foundation
+      </Copyright>
+    </LandingPageComponent>
   );
 }
 
--- src/pages/LandingPage/landingpage-style.js
+++ src/pages/LandingPage/landingpage-style.js
@@ -3,20 +3,52 @@ import styled from "styled-components";
 const nhfBlue = "#4a90e2";
 // const nhfYellow = "#e7ff00";
 
+export const LandingPageComponent = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const LandingPageContainer = styled.div`
+  width: 100%;
+  background-color: #4a90e2;
+  display: flex;
+  flex-direction: row;
+
+  @media only screen and (max-width: 786px) {
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
 export const VolunterSplash = styled.div`
   display: flex;
   flex-direction: column;
-  height: 100vh;
   background-color: ${nhfBlue};
   width: 56%;
+  height: 100vh;
+
+  @media only screen and (max-width: 786px) {
+    display: flex;
+    flex-direction: column;
+    width: 100vw;
+    height: 100vh;
+  }
 `;
 
 export const Copyright = styled.div`
   color: #fff;
-  position: absolute;
-  bottom: 2%;
-  margin-left: 2rem;
   font-size: 0.8rem;
+  position: absolute;
+  bottom: 3rem;
+  margin-left: 2rem;
+
+  @media only screen and (max-width: 786px) {
+    position: relative;
+    color: ${nhfBlue};
+    margin-left: 0;
+    align-self: center;
+    bottom: 1rem;
+  }
 `;
 
 export const TitleText = styled.h1`
@@ -26,6 +58,13 @@ export const TitleText = styled.h1`
   font-size: 7rem;
   font-weight: 400;
   margin-top: 12rem;
+
+  @media only screen and (max-width: 786px) {
+    font-size: 5rem;
+    margin-left: 3rem;
+    font-size: 4.14rem;
+    margin-top: 10rem;
+  }
 `;
 
 export const SubtitleText = styled.h4`
@@ -40,6 +79,13 @@ export const SubtitleText = styled.h4`
     font-family: "Roboto Slab";
     font-style: normal;
   }
+
+  @media only screen and (max-width: 786px) {
+    font-size: ${({ type }) =>
+      type === "other-words" ? "0.863rem" : "1.4rem"};
+    margin-left: 3rem;
+    margin-top: ${({ type }) => (type === "other-words" ? "1.4rem" : "0.1rem")};
+  }
 `;
 
 export const Definition = styled.p`
@@ -50,16 +96,35 @@ export const Definition = styled.p`
   font-size: 1.4rem;
   line-height: 1.5;
   margin-top: 2rem;
+
+  @media only screen and (max-width: 786px) {
+    margin-left: 3rem;
+    font-size: 0.863rem;
+    line-height: 1.294rem;
+    width: 76%;
+  }
 `;
 
 export const WarningWrapper = styled.div`
-  height: 100vh;
   background-color: #fff;
   width: 44%;
+  height: 100vh;
+
+  @media only screen and (max-width: 786px) {
+    display: flex;
+    width: 100%;
+    height: 100vh;
+  }
 `;
 
 export const Logo = styled.img`
   width: 150px;
   margin-left: 4rem;
   margin-top: 5rem;
+
+  @media only screen and (max-width: 786px) {
+    width: 100px;
+    margin-left: 2rem;
+    margin-top: 3rem;
+  }
 `;
