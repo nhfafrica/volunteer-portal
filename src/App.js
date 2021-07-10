75f5c401730a0aa2fd81649dd19f3da3826ae1d0
Benson Imoh,ST <benson@stbensonimoh.com>
2021-07-10 12:15:42 +0100

    build: :heavy_plus_sign: Add WebFontLoader
    - Load the necessary Google fonts that the application needs.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import { useEffect } from "react";
+import WebFont from "webfontloader";
 import { GlobalStyle, MainContent } from "./global-styles";
 import { LandingPage } from "./pages";
 import "./App.css";
 
 function App() {
+  //Load the fonts
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: [
+          "Old Standard TT",
+          "Dosis",
+          "Roboto Slab",
+          "Roboto Condensed",
+        ],
+      },
+    });
+  }, []);
+
   return (
     <div className="App">
       <GlobalStyle />
