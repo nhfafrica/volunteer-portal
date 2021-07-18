f6371c193dd6fc79cc72b2900d613f263538bb38
Benson Imoh,ST <benson@stbensonimoh.com>
2021-07-18 17:09:05 +0100

    refactor: :art: Move the styles into the component file

--- src/pages/landing/LandingPage.js
+++ src/pages/landing/LandingPage.js
@@ -1,27 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 import Button from "../../components/Button";
 import Copyright from "../../components/Copyright";
 import Logo from "../../components/Logo";
-import {
-  LandingPageComponent,
-  LandingPageContainer,
-  VolunterSplash,
-  TitleText,
-  SubtitleText,
-  Definition,
-  WarningWrapper,
-  WarningSubtitle,
-  WarningTitle,
-  DefinitionContainer,
-  WarningText,
-} from "./landingpage-style";
+const nhfBlue = "#4a90e2";
 
 function LandingPage() {
   return (
     <LandingPageComponent>
       <LandingPageContainer>
-        <Logo/>
+        <Logo />
         <VolunterSplash>
           <DefinitionContainer>
             <TitleText>Volunteer</TitleText>
@@ -93,5 +82,191 @@ function LandingPage() {
     </LandingPageComponent>
   );
 }
+
+// STYLES
+const LandingPageComponent = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const LandingPageContainer = styled.div`
+  width: 100%;
+  background-color: #4a90e2;
+  display: flex;
+  flex-direction: row;
+
+  @media only screen and (max-width: 786px) {
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
+const VolunterSplash = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: ${nhfBlue};
+  width: 56%;
+  height: 100vh;
+  justify-content: center;
+
+  @media only screen and (max-width: 786px) {
+    display: flex;
+    flex-direction: column;
+    width: 100vw;
+    height: 100vh;
+    justify-content: center;
+  }
+`;
+
+const DefinitionContainer = styled.div`
+  @media only screen and (max-width: 786px) {
+    display: flex;
+    flex-direction: column;
+    align-self: flex-end;
+  }
+`;
+
+const TitleText = styled.h1`
+  font-family: "Roboto Slab";
+  color: #fff;
+  margin-left: 8rem;
+  font-size: 7rem;
+  font-weight: 400;
+  margin-top: 12rem;
+
+  @media only screen and (max-width: 1024px) {
+    font-size: 6rem;
+    margin-left: 4rem;
+  }
+
+  @media only screen and (max-width: 786px) {
+    font-size: 5rem;
+    margin-left: 3rem;
+    font-size: 4.14rem;
+    margin-top: 0rem;
+  }
+`;
+
+const SubtitleText = styled.h4`
+  font-family: "Old Standard TT";
+  color: #fff;
+  margin-left: 8rem;
+  margin-top: ${({ type }) => (type === "other-words" ? "2rem" : "0.1rem")};
+  font-size: ${({ type }) => (type === "other-words" ? "1.4rem" : "2rem")};
+  font-style: italic;
+
+  & > span {
+    font-family: "Roboto Slab";
+    font-style: normal;
+  }
+
+  @media only screen and (max-width: 1024px) {
+    margin-left: 4rem;
+  }
+
+  @media only screen and (max-width: 786px) {
+    font-size: ${({ type }) =>
+      type === "other-words" ? "0.863rem" : "1.4rem"};
+    margin-left: 3rem;
+    margin-top: ${({ type }) => (type === "other-words" ? "1.4rem" : "0.1rem")};
+  }
+`;
+
+const Definition = styled.p`
+  font-family: "Roboto Slab";
+  margin-left: 8rem;
+  color: #fff;
+  width: 30rem;
+  font-size: 1.4rem;
+  line-height: 1.5;
+  margin-top: 2rem;
+
+  @media only screen and (max-width: 1024px) {
+    margin-left: 4rem;
+    width: 76%;
+    font-size: 1.2rem;
+  }
+
+  @media only screen and (max-width: 786px) {
+    margin-left: 3rem;
+    font-size: 1.2rem;
+    width: 76%;
+  }
+`;
+
+const WarningWrapper = styled.div`
+  background-color: #fff;
+  width: 44%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  @media only screen and (max-width: 1024px) {
+    padding-bottom: 2rem;
+  }
+
+  @media only screen and (max-width: 786px) {
+    display: flex;
+    width: 100%;
+    height: 100%;
+    padding-bottom: 4rem;
+  }
+`;
+
+const WarningSubtitle = styled.h4`
+  text-transform: uppercase;
+  font-family: "Roboto Condensed";
+  margin-top: 6.563rem;
+
+  @media only screen and (max-width: 1024px) {
+    margin-top: 3rem;
+  }
+
+  @media only screen and (max-width: 786px) {
+    margin-top: 4rem;
+  }
+`;
+
+const WarningTitle = styled.h1`
+  text-transform: uppercase;
+  font-family: "PT Sans";
+  font-size: 1.5rem;
+  color: ${nhfBlue};
+
+  @media only screen and (max-width: 786px) {
+    margin-top: 5px;
+  }
+`;
+
+const WarningText = styled.div`
+  font-family: "Roboto";
+  font-size: 1rem;
+  line-height: 1.172rem;
+  margin-left: 6rem;
+  margin-right: 6rem;
+  margin-top: 2rem;
+
+  & > p {
+    margin-top: 2rem;
+  }
+
+  @media only screen and (max-width: 1024px) {
+    margin-left: 2.5rem;
+    margin-right: 2.5rem;
+    margin-top: 1rem;
+    font-size: 0.9rem;
+
+    & > p {
+      margin-top: 0.7rem;
+    }
+  }
+
+  @media only screen and (max-width: 786px) {
+    margin-top: 0.5rem;
+    margin-left: 2.5rem;
+    margin-right: 2.5rem;
+  }
+`;
 
 export default LandingPage;
